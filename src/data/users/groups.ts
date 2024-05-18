9596b8da0a9722782a995ad071522089a1aa0504
JuandiGo1 <98894997+JuandiGo1@users.noreply.github.com>
2024-05-17 19:14:57 -0500

    Update groups.ts

--- src/data/users/groups.ts
+++ src/data/users/groups.ts
@@ -31,3 +31,66 @@ export async function getGroupJoinRequests(userId: string) {
         return { error: 'Error getting group join requests!' }
     }
 }
+
+
+export async function getGroupInvitations(userId: string) {
+    try {
+        const invitations = await db.groupInvitation.findMany({
+            where: {
+                userId: userId
+            },
+            include: {
+                group: {
+                    select: {
+                        id: true,
+                        name: true,
+                        description: true
+                    }
+                },
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true,
+                        image: true
+                    }
+                }
+            }
+        })
+
+        return { success: 'Group invitations found!', data: invitations }
+    } catch (err) {
+        console.log(err)
+        return { error: 'Error getting group invitations!' }
+    }
+}
+
+export async function getUserGroups(userId: string) {
+    try {
+        const memberships = await db.groupMembership.findMany({
+            where: {
+                userId: userId
+            },
+            include: {
+                group: {
+                    select: {
+                        id: true,
+                        name: true,
+                        description: true,
+                        adminId: true,
+                        createdAt: true,
+                        updatedAt: true
+                    }
+                }
+            }
+        })
+
+        const groups = memberships.map((membership: { group: any }) => membership.group)
+
+
+        return { success: 'Groups found!', data: groups }
+    } catch (err) {
+        console.log(err)
+        return { error: 'Error getting user groups!' }
+    }
+}
